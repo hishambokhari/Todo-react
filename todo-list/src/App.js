6572698d057a1599624e2d1fa17953bbd516552a
root <hish.b92@gmail.com>
2020-02-26 18:48:20 +0000

    Add: CSS/styling

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,10 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+  }
+  render() {
   return (
-    <h1>Hello</h1>
+    <div className="App">
+      <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter Text"
+          value={this.state.currentItem.text}
+          onChange={this.handleInput}/>
+          <button type="submit">Add</button>
+          </form>
+      </header>
+    </div>
   );
+  }
 }
+
 
 export default App;
